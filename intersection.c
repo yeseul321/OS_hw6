550099fb14e5d0dd955b364997bb34fd07277247
yeseul321 <amy7806@gmail.com>
2021-12-02 23:43:31 +0900

    final

--- intersection.c
+++ intersection.c
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+// #define DEBUG 1
+
 #define NO_CAR 9
 #define VERTICAL 1
 #define HORIZONTAL 0
@@ -13,9 +15,32 @@
 #define SOUTH 3
 #define EAST 4
 
+struct stat {
+        int dir;
+        int time;
+        int way;
+};
+
+typedef struct car{
+        int deg;
+        struct stat stat[2];
+} car;
+
+typedef struct waiting{
+        int data;
+        int index;
+        struct waiting *next;
+} waiting_way;
+
+typedef struct waiting_all{
+        int data;
+        struct waiting_all *next;
+} waiting_list;
+
 void *thread_func(void *arg);
 
-bool is_empty_way(int *arr);
+//bool is_empty_way(int *arr);
+bool is_empty_way(struct waiting *waiting);
 bool is_vertical(int start);
 bool is_same_dir(int startWay);
 
@@ -25,8 +50,15 @@ int check_passed_car(void);
 void check_no_car(void);
 void print_situ_result(void);
 bool is_work_all_finished(void);
-void print_result(void);
+void print_result(int tick);
+void way_addData(struct waiting *waiting, int data, int index);
+void all_addData(struct waiting_all *waiting, int data);
+int check_waiting_length(struct waiting *waiting);
+int setWaylist(struct waiting *waiting,int random);
+void setAlllist(struct waiting_all *head, int index);
 
+
+/*
 struct stat {
 	int dir;
 	int time;
@@ -39,10 +71,16 @@ typedef struct car{
 } car;
 
 typedef struct waiting{
-	int car;
-	int index_queue;
+	int data;
+	int index;
+	struct waiting *next;
 } waiting_way;
 
+typedef struct waiting_all{
+	int data;
+	struct waiting_all *next;
+} waiting_list;
+*/
 int ticks = 0;
 int vehicles = 0;
 int *startPoint;
@@ -50,16 +88,35 @@ int *wait_queue;
 int way_passed[4] = {0,};
 int passed_car = 0;
 //int thr1Point[15] = {0,},thr2Point[15]={0,},thr3Point[15]={0,},thr4Point[15]={0,};
+int *all_waiting_list;
 int *p_startPoint[4];
+int *each_passed;
 bool finish_active[4] = {false, false, false, false};
 
 car running_car = {9, {{0,0,0},{0,0,0}}};
-waiting_way way_waiting_car[4] = {{0,0},{0,0},{0,0},{0,0}};
+//waiting_list *all_head;
+//all_head->next = NULL;
+
+waiting_way *way_head[4];
+/*
+for(int i = 0;i<4;i++){
+	way_head[i] = malloc(sizeof(struct waiting_way));
+	way_head[i]->next = NULL;
+}
+*/
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 int main(void){
+	//all_head = malloc(sizeof(struct waiting_all));
+	//all_head->next = NULL;
+	each_passed = calloc(4, sizeof(int));
+	for(int i = 0;i<4;i++){
+        way_head[i] = malloc(sizeof(struct waiting));
+        way_head[i]->next = NULL;
+	all_waiting_list = malloc(sizeof(int)*16);
+}
 	/***********************************************************/
 	//Enter the number and initializing startPoint Array
 	printf("Total number of vehicles : ");
@@ -68,20 +125,26 @@ int main(void){
 	for(int i = 0;i < 4;i++){
 		p_startPoint[i] = (int *)calloc(15, sizeof(int));
 	}
-
 	srand((unsigned)time(NULL));
 	printf("Start Point : ");
+#ifdef DEBUG
+	//startPoint = {2,3,3,4,4,3,4,2,2,2};
+	for(int i = 0;i<vehicles;i++){
+		printf("%d ", startPoint[i]);
+	}
+#else
 	for(int i=0;i<vehicles;i++){
 		startPoint[i] = (rand() % 4) + 1;
 		printf("%d ", startPoint[i]);
 	}
+#endif
 	printf("\n");
 	/***********************************************************/
 	//pthread create
 	pthread_t tid[4];
 	int way_num[4] = {0, };
-	for(int i=0;i < 4;i++){
-		way_num[i] = i+1;
+	for(int i=4;i > 0;i--){
+		way_num[i] = i;
 		if(pthread_create(&tid[i], NULL, thread_func, (void *)&way_num[i]) != 0){
 			fprintf(stderr, "pthread_create error\n");
 			exit(1);
@@ -93,48 +156,52 @@ int main(void){
 	int one=0, two=0, three=0, four=0, tick = 0;
 	while(1){   //-> until ticks ends
 		sleep(1);
-		//printf("===============================\n");
 		printf("tick %d\n", tick);
 		printf("===============================\n");
-		//pthread_cond_broadcast(&cond);
 
 		if(tick<vehicles){
 				if(startPoint[tick] == 1){
-					p_startPoint[0][one] = startPoint[tick];
+					way_addData(way_head[0],1,tick);
+					all_waiting_list[tick] = 1;
 					one++;
 				}
 				else if(startPoint[tick] == 2){
-					p_startPoint[1][two] = startPoint[tick];
+					way_addData(way_head[1],2,tick);
+					all_waiting_list[tick] = 2;
 					two++;
                                 }
 				else if(startPoint[tick] == 3){
-                                        p_startPoint[2][three] = startPoint[tick];
+					way_addData(way_head[2],3,tick);
+					all_waiting_list[tick] = 3;
 					three++;
                                 }
 				else if(startPoint[tick] == 4){
-                                        p_startPoint[3][four] = startPoint[tick];
+					way_addData(way_head[3],4,tick);
+					all_waiting_list[tick] = 4;
 					four++;
                                 }
 			
-			tick++;
 		}
-		else break;
 		pthread_cond_broadcast(&cond);
 		until_finish_active();
 		update_time();
-		if((passed_car = check_passed_car())!=0){
-			printf("passed car : %d\n", passed_car);
-		}
 		print_situ_result();
 		printf("===============================\n");
 		check_no_car();
 
-		if(is_work_all_finished() == true){
-			print_situ_result();
-			break;
+		if(tick>=vehicles){
+			if(is_work_all_finished() == true){
+				sleep(1);
+				printf("tick : %d\n", ++tick);
+                		printf("===============================\n");
+				print_situ_result();
+				break;
+			}
 		}
+		tick++;
 	}
-	print_result();
+	printf("===============================\n");
+	print_result(tick);
 
 	return 0;
 }
@@ -143,54 +210,62 @@ int main(void){
 void *thread_func(void *arg){
 	int startWay = *((int *)arg);
 	int way_waiting = 0;
+	int waiting_length = 0;
+	int random = 0;
+	int index = 0;
+	srand((unsigned)time(NULL));
 	while(1){
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&mutex);
 		pthread_cond_wait(&cond, &mutex);
-		//printf("%d -> ", startWay);
-		//for(int i = 0;i<15;i++){
-		//	printf("%d ", p_startPoint[startWay-1][i]);
-		//}
-		//printf("\n");
 
-		if(!is_empty_way(p_startPoint[startWay-1]) && !is_vertical(startWay))
+		//printf("i am %d\n", startWay);
+
+		if(!is_empty_way(way_head[startWay-1]) && !is_vertical(startWay))
 		{
+			waiting_length = check_waiting_length(way_head[startWay-1]);
 			//EMPTY ROAD
 			if(running_car.deg == NO_CAR)
 			{
-				printf("EMPTY\n");
 				running_car.deg = startWay % 2;
 				running_car.stat[0].dir = startWay;
 				//대기큐 랜덤 뽑기해서 넣기
-				running_car.stat[0].way = p_startPoint[startWay-1][way_waiting];
+				random = rand()%(waiting_length-1)+1;
+				running_car.stat[0].way = startWay;
+
 				//랜덤뽑기한 후 대기큐에 대한 셋팅
+				index = setWaylist(way_head[startWay-1],random);
+				all_waiting_list[index] = 0;
 				running_car.stat[0].time = 0;
-				//이건 아마 없애도 될 듯
-				p_startPoint[startWay-1][way_waiting] = 0;
+
 				way_waiting++;
 			}
 			//FACE-TO-FACE CAR
 			else if((running_car.deg == startWay % 2) && (!is_same_dir(startWay)))
 			{
-			  if((running_car.stat[0].dir!=0&&running_car.stat[0].time!=0) || (running_car.stat[0].dir!=0&&running_car.stat[0].time!=0))
+			  if((running_car.stat[0].dir!=0&&running_car.stat[0].time!=0) || (running_car.stat[1].dir!=0&&running_car.stat[1].time!=0))
 			  {
-				printf("start way : %d\n", startWay);
 				if(running_car.stat[0].way==0){
-					//
-					running_car.stat[0].way = p_startPoint[startWay-1][way_waiting];
-					//
+					//랜덤
+					random = rand()%(waiting_length-1)+1;
+                                	running_car.stat[0].way = startWay;
+
+					//랜덤뽑기한 후 대기큐에 대한 셋팅
+                                	index = setWaylist(way_head[startWay-1],random);
+					all_waiting_list[index] = 0;
 					running_car.stat[0].dir = startWay;
 					running_car.stat[0].time = 0;
-					//
-					p_startPoint[startWay-1][way_waiting] = 0;
 				}
 				else if(running_car.stat[1].way==0){
-					//
-                                        running_car.stat[1].way = p_startPoint[startWay-1][way_waiting];
-					//
+					//랜덤
+                                        random = rand()%(waiting_length-1)+1;
+                                        running_car.stat[1].way = startWay;
+                                        
+					//랜덤뽑기한 후 대기큐에 대한 셋팅
+                                        index = setWaylist(way_head[startWay-1],random);
+					all_waiting_list[index] = 0;
                                         running_car.stat[1].dir = startWay;
                                         running_car.stat[1].time = 0;
-					//
-					p_startPoint[startWay-1][way_waiting] = 0;
+					
                                 }
 				way_waiting++;
 			  }
@@ -199,24 +274,19 @@ void *thread_func(void *arg){
 			}
 		}
 		else{//lock을 얻을 조건이 안될 때
-			//각 방향 대기큐에 차 넣어주고 전체 대기큐에도 넣어줌.
-			//각 방향 대기큐 숫자 늘리기
 		}
-		//way_waiting++;
+
 		finish_active[startWay-1] = true;
 		pthread_mutex_unlock(&mutex);
 	}
 	return NULL;
 }
 
-bool is_empty_way(int *arr){
-	for(int i = 0;i < 15;i++){
-		if(arr[i] == 0)
-			continue;
-		else
-			return false;
+bool is_empty_way(struct waiting *way_waiting){
+	if(way_waiting->next == NULL){
+		return true;
 	}
-	return true;
+	else return false;
 }
 bool is_vertical(int start){
 	if(running_car.deg == (start % 2)) return false; //Same Degree
@@ -241,7 +311,7 @@ void until_finish_active(void){
 			}
 		}
 		if(finished == true){
-			printf("all finish\n");
+			//printf("all finish\n");
 			break;
 		}
 	}
@@ -250,11 +320,9 @@ void until_finish_active(void){
 void update_time(void){
 	for(int i = 0;i < 2;i++){
 		if(running_car.stat[i].way != 0){
-			printf("car %d is updated to %d\n", running_car.stat[i].way, running_car.stat[i].time+1);
 			running_car.stat[i].time++;
 		}
 	}
-	//printf("update\n");
 	return;
 }
 
@@ -263,6 +331,7 @@ int check_passed_car(void){
 	for(int i = 0; i<2;i++){
 		if(running_car.stat[i].time == 2){
 			pass_car = running_car.stat[i].way;
+			each_passed[running_car.stat[i].dir-1]++;
 			running_car.stat[i].dir = 0;
 			running_car.stat[i].time = 0;
 			running_car.stat[i].way = 0;
@@ -276,11 +345,141 @@ void check_no_car(void){
 		running_car.deg = NO_CAR;
 }
 void print_situ_result(void){
-	//각 tick마다 출력될 결과들 여기서 출력
+	int pass = 0;
+	struct waiting_all *this = malloc(sizeof(struct waiting_all));
+        //this = all_head->next;
+	
+	printf("Passed Vehicle\n");
+	printf("Car ");
+	if((passed_car = check_passed_car())!=0){
+                        printf("%d\n", passed_car);
+                }
+	else printf("\n");
+
+	printf("Waiting Vehicle\n");
+	printf("Car ");
+	for(int i = 0;i < 16; i++){
+		if(all_waiting_list[i] != 0)
+			printf("%d ", all_waiting_list[i]);
+	}
+	printf("\n");
 }
 bool is_work_all_finished(void){
-	//모든 일이 끝났는지 확인. 도로가 비어있고 대기큐에 아무것도 없는지
+	bool ret = true;
+	for(int i = 0; i<16; i++){
+		if(all_waiting_list[i] != 0)
+			ret = false;
+	}
+	if(running_car.deg != NO_CAR)
+		ret = false;
+
+	return ret;
 }
-void print_result(void){
-	//제일 마지막 결과 출력해주는 함수
+void print_result(int tick){
+	printf("Number of vehicles passed from each start point\n");
+	for(int i = 0;i < 4; i++){
+		printf("P%d : %d times\n", i+1, each_passed[i]);
+	}
+	printf("Total time : %d ticks\n", tick);
+}
+void way_addData(struct waiting *waiting, int data, int index){
+	struct waiting *newNode = malloc(sizeof(struct waiting));
+	newNode->next = waiting->next;
+	newNode->data = data;
+	newNode->index = index;
+
+	waiting->next = newNode;
+}
+void all_addData(struct waiting_all *waiting, int data){
+	struct waiting_all *newNode = malloc(sizeof(struct waiting_all));
+	struct waiting_all *this = malloc(sizeof(struct waiting_all));
+	this = waiting;
+	newNode->next = NULL;
+	if(waiting->next = NULL) {
+		waiting->next = newNode;
+		newNode->data = data;
+	}
+	else{
+		while(this->next != NULL){
+                this = this -> next;
+		}
+		this->next = newNode;
+		newNode->data = data;
+        }
+
+        //newNode->next = waiting->next;
+        //newNode->data = data;
+
+        //waiting->next = newNode;
+}
+int check_waiting_length(struct waiting *head){
+	int count = 0;
+	struct waiting *this = malloc(sizeof(struct waiting));
+	this = head;
+	while(this != NULL){
+		count++;
+		this = this -> next;
+	}
+	return count;
+}
+int setWaylist(struct waiting *head,int random){
+	struct waiting *cur_node = malloc(sizeof(struct waiting));
+	struct waiting *pre_node = malloc(sizeof(struct waiting));
+	int i = 1;
+	int len = check_waiting_length(head);
+	int ret = 0;
+	if(random==1){ 
+		if(len == 1){
+			ret = head->next->index;
+			head->next = NULL;
+		}
+		else{
+			ret = head->next->index;
+			head->next = head->next->next;
+		}
+	}
+	else{
+		pre_node = head;
+		cur_node = pre_node->next;
+		while((cur_node != NULL)&&(i<random-1)){
+			i++;
+			pre_node = cur_node;
+			if(cur_node->next == NULL){
+				ret = pre_node->index;
+				pre_node->next = NULL;
+				break;
+			}
+			else cur_node = cur_node->next;
+		}
+		if(pre_node->next != NULL){
+			ret = cur_node->index;
+			pre_node->next = cur_node->next;
+		}
+	}
+	return ret;
+}
+
+	
+void setAlllist(struct waiting_all *head, int index){
+	struct waiting_all *cur_node = malloc(sizeof(struct waiting_all));
+        struct waiting_all *pre_node = malloc(sizeof(struct waiting_all));
+        int i = 1;
+        if(index==1) head->next = NULL;
+        else{
+                pre_node = head;
+                cur_node = pre_node->next;
+                while((cur_node != NULL)&&(i<index-1)){
+                        i++;
+                        pre_node = cur_node;
+                        if(cur_node->next == NULL){
+                                pre_node->next = NULL;
+                                break;
+                        }
+                        else cur_node = cur_node->next;
+                }
+                if(pre_node->next != NULL){
+                        pre_node->next = cur_node->next;
+                }
+        }
+
 }
